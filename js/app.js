199b402db28ba5b5c17292e45b67e41585791c60
MaanRaddadi <maan.raddadi@gmail.com>
2023-10-11 18:53:28 +0300

    - added a method to toggle nav bar on scroll - added scroll behavior to navbar buttons - added scroll to top button - added a method to only show the scroll to top button when the user is at the end of the page

--- js/app.js
+++ js/app.js
@@ -18,42 +18,41 @@
  * Great to have comments before crucial code sections within the procedure.
  */
 
-/**
- * Define Global Variables
- *
- */
+//Define Global Variables
+
+const navBar = document.querySelector(".navbar__menu");
 const navList = document.getElementById("navbar__list");
 const sectionsList = document.querySelectorAll("section");
-
-let navClone;
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
+const header = document.querySelector(".page__header");
+const footer = document.querySelector(".page__footer");
+const scrollBtn = document.getElementById("scrollBtn");
 
 // build the nav
 function navBuilder() {
   sectionsList.forEach((section) => {
-    navClone += `<li><a class="menu__link" id="navli" href="#${section.id}">${section.dataset.nav}</a></li>`;
+    //For each exsiting section add a li element
+    const navLink = document.createElement("li");
+    // Adding the corresponding id to each nav link and the corresponding dataset
+    navLink.insertAdjacentHTML(
+      "afterbegin",
+      `<a class="menu__link" id="navli" href="#${section.id}">${section.dataset.nav}</a>`
+    );
+
+    // Add the generated nav link to its ul
+    navList.appendChild(navLink);
+
+    // Adding scroll action to nav link
+    scrollToSection(navLink, section);
   });
-
-  navList.innerHTML = navClone;
+  navBar.appendChild(navList);
 }
-
-navBuilder();
 
 // Add class 'active' to section when near top of viewport
 function makeActive() {
   sectionsList.forEach((section) => {
+    // Get the relative position for each section
     const box = section.getBoundingClientRect();
-
+    // if the section is in position make it active
     if (box.top <= 150 && box.bottom >= 150) {
       isActive(section);
     } else {
@@ -64,27 +63,75 @@ function makeActive() {
 
 makeActive();
 
+window.addEventListener("scroll", (e) => {
+  // active section
+  makeActive();
+  // nav bar visiability
+  toggleNavbar();
+  // scroll button
+  scrollTop();
+});
+
 // Scroll to anchor ID using scrollTO event
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
+function scrollToSection(navLink, section) {
+  // Adding "click" event to the nav bar link
+  navLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (section) {
+      // Scroll to the target section smoothly
+      window.scrollTo({
+        top: section.offsetTop,
+        behavior: "smooth",
+      });
+    }
+  });
+}
+// toggling the header to only show when user scrolls
+function toggleNavbar() {
+  // timeout id for calling clearTimeout
+  let userScroll;
+  // onScroll show the header
+  header.style.cssText = "opacity: 1; transition: ease 0.3s;";
+  // clear any previus timeouts set by setTimeout()
+  window.clearTimeout(userScroll);
+  // calling setTimeout() with the id, after 5000ms of the user not scrolling hide the header
+  userScroll = setTimeout(() => {
+    header.style.cssText = "opacity: 0; transition: ease 0.3s;";
+  }, 5000);
+}
 
 // Set sections as active
 function isActive(section) {
+  // getting id for the active class
   const id = section.getAttribute("id");
-
+  // add  active class "your-active-class" to the current active section
   document.getElementById(id).classList.add("your-active-class");
 }
 
 function isNonActive(section) {
+  // getting the id for the active class
   const id = section.getAttribute("id");
-
+  // add active class "your-active-class" to current active section
   document.getElementById(id).classList.remove("your-active-class");
+}
+
+// scroll button event
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+// adding scroll to top button
+
+function scrollTop() {
+  // footer offset where the button will be positioned
+  let footerOffset = footer.offsetTop - 640;
+  let currentUsrScroll = window.scrollY;
+  // if the user scrolls pass the offset the button will show, else it will be hidden
+  if (currentUsrScroll >= footerOffset) {
+    scrollBtn.classList.remove("hidden");
+  } else {
+    scrollBtn.classList.add("hidden");
+  }
 }
